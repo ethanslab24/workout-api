5aecfe3b1d7f8978aa5e8847463760dbd49f7bf4
ethan <eduena4@wgu.edu>
2026-06-04 19:05:13 -0500

    Add workout creation form and frontend validation

--- workout-frontend/src/App.jsx
+++ workout-frontend/src/App.jsx
@@ -2,21 +2,90 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [exerciseName, setExerciseName] = useState("")
+  const [sets, setSets] = useState("")
+  const [reps, setReps] = useState("")
+  const [weight, setWeight] = useState("")
+  const [date, setDate] = useState("")
   const [workouts, setWorkouts] = useState([])
 
-useEffect(() => {
-  fetch('http://localhost:8080/workouts')
-    .then(response => response.json())
-    .then(data => {
+  useEffect(() => {
+    fetch('http://localhost:8080/workouts')
+      .then(response => response.json())
+      .then(data => {
       console.log(data)
       setWorkouts(data)
     })
-}, [])
-  
+  }, [])
 
+  function addWorkout() {
+
+    if (!Number.isInteger(Number(sets)) || !Number.isInteger(Number(reps))) {
+        alert("Sets and reps must be whole numbers.")
+        return
+    }
+
+    const addedWorkout = {
+          exerciseName,
+          sets: Number(sets),
+          reps: Number(reps),
+          weight: Number(weight),
+          date
+    }
+    console.log(addedWorkout)
+    fetch('http://localhost:8080/workouts', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(addedWorkout)
+    }).then(response => {
+      if (!response.ok) {
+          throw new Error("Failed to add workout")
+      }
+      return response.json()}).then(savedWorkout => {
+      setWorkouts([...workouts, savedWorkout])})
+  }
+  
   return (
     <main>
       <h1>Workout Tracker</h1>
+
+      <input
+       type="text"
+       placeholder="Exercise Name"
+       value={exerciseName}
+       onChange={(event) => setExerciseName(event.target.value)}
+      />
+
+      <input
+       type="number"
+       step="1"
+       placeholder="Sets"
+       value={sets}
+       onChange={(event) => setSets(event.target.value)}
+      />
+
+      <input
+       type="number"
+       step="1"
+       placeholder="Reps"
+       value={reps}
+       onChange={(event) => setReps(event.target.value)}
+      />
+
+      <input
+       type="number"
+       placeholder="Weight"
+       value={weight}
+       onChange={(event) => setWeight(event.target.value)}
+      />
+
+      <input
+       type="date"
+       value={date}
+       onChange={(event) => setDate(event.target.value)}
+      />
+
+      <button onClick={addWorkout}>Add Workout</button>
 
       <section>
         <h2>Workouts</h2>
